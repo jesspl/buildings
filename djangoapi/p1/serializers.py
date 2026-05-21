95f5c2cf4c958e5aa97ec22076cc447c5503d085
jesspl <leucauge@gmail.com>
2026-05-21 14:39:29 +0200

    Cap 8 - check_st_relation serializers p1

--- djangoapi/p1/serializers.py
+++ djangoapi/p1/serializers.py
@@ -3,24 +3,27 @@ from .models import Calles, Semaforos, Manzanas
 
 class CallesSerializer(GeoModelSerializer):
     check_geometry_is_valid = True
-    check_st_relation = False
-
+    check_st_relation = True
+    matrix9IM = 'T********'
     class Meta:
         model = Calles
         fields = GeoModelSerializer.Meta.fields + ['nombre', 'longitud', 'estado']
+    def get_table_name(self):
+        return '"p1"."calles"'
 
 class SemaforosSerializer(GeoModelSerializer):
     check_geometry_is_valid = True
     check_st_relation = False
-
     class Meta:
         model = Semaforos
         fields = GeoModelSerializer.Meta.fields + ['nombre', 'estado', 'tipo']
 
 class ManzanasSerializer(GeoModelSerializer):
     check_geometry_is_valid = True
-    check_st_relation = False
-
+    check_st_relation = True
+    matrix9IM = 'T********'
     class Meta:
         model = Manzanas
         fields = GeoModelSerializer.Meta.fields + ['nombre', 'area', 'descripcion']
+    def get_table_name(self):
+        return '"p1"."manzanas"'
