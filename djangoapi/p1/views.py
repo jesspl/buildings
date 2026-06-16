db1b7c96df260d07cd4fbff102928df148d2a1fd
jesspl <leucauge@gmail.com>
2026-06-16 12:59:04 +0200

    Eval 3 - LoginRequiredMixin IsAuthenticated p1 views

--- djangoapi/p1/views.py
+++ djangoapi/p1/views.py
@@ -1,21 +1,45 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# # Create your views here.
+# from rest_framework import viewsets, permissions
+# from p1.models import Calles, Semaforos, Manzanas
+# from p1.serializers import CallesSerializer, SemaforosSerializer, ManzanasSerializer
+
+# class CallesModelViewSet(viewsets.ModelViewSet):
+#     queryset = Calles.objects.all()
+#     serializer_class = CallesSerializer
+#     permission_classes = [permissions.AllowAny]
+
+# class SemaforosModelViewSet(viewsets.ModelViewSet):
+#     queryset = Semaforos.objects.all()
+#     serializer_class = SemaforosSerializer
+#     permission_classes = [permissions.AllowAny]
+
+# class ManzanasModelViewSet(viewsets.ModelViewSet):
+#     queryset = Manzanas.objects.all()
+#     serializer_class = ManzanasSerializer
+#     permission_classes = [permissions.AllowAny]
+
+from django.shortcuts import render
 from rest_framework import viewsets, permissions
+from rest_framework.authentication import SessionAuthentication
 from p1.models import Calles, Semaforos, Manzanas
 from p1.serializers import CallesSerializer, SemaforosSerializer, ManzanasSerializer
 
 class CallesModelViewSet(viewsets.ModelViewSet):
     queryset = Calles.objects.all()
     serializer_class = CallesSerializer
-    permission_classes = [permissions.AllowAny]
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
 
 class SemaforosModelViewSet(viewsets.ModelViewSet):
     queryset = Semaforos.objects.all()
     serializer_class = SemaforosSerializer
-    permission_classes = [permissions.AllowAny]
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
 
 class ManzanasModelViewSet(viewsets.ModelViewSet):
     queryset = Manzanas.objects.all()
     serializer_class = ManzanasSerializer
-    permission_classes = [permissions.AllowAny]
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
