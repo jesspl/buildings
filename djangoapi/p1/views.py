a5539f2480d5073f535714c41931cada690092b7
jesspl <leucauge@gmail.com>
2026-06-16 16:55:33 +0200

    Eval 3 - views p1 final

--- djangoapi/p1/views.py
+++ djangoapi/p1/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # # Create your views here.
 # from rest_framework import viewsets, permissions
@@ -20,26 +20,21 @@
 #     serializer_class = ManzanasSerializer
 #     permission_classes = [permissions.AllowAny]
 
-from django.shortcuts import render
 from rest_framework import viewsets, permissions
-from rest_framework.authentication import SessionAuthentication
 from p1.models import Calles, Semaforos, Manzanas
 from p1.serializers import CallesSerializer, SemaforosSerializer, ManzanasSerializer
 
 class CallesModelViewSet(viewsets.ModelViewSet):
     queryset = Calles.objects.all()
     serializer_class = CallesSerializer
-    authentication_classes = [SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 class SemaforosModelViewSet(viewsets.ModelViewSet):
     queryset = Semaforos.objects.all()
     serializer_class = SemaforosSerializer
-    authentication_classes = [SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 class ManzanasModelViewSet(viewsets.ModelViewSet):
     queryset = Manzanas.objects.all()
     serializer_class = ManzanasSerializer
-    authentication_classes = [SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
